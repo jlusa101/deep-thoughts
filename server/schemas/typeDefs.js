347abcdd077ab224a141ac587225f96f07bde12e
Joona Lusa <joona.lusa@hotmail.com>
2022-03-15 19:57:13 -0400

    created schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,8 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-  type Query {
-    helloWorld: String
+type Query {
+    thoughts: [Thought]
   }
 `;
 
