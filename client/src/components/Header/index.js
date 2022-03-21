f50d3fd319f16da5517ea840dfe52de789433a13
Joona Lusa <joona.lusa@hotmail.com>
2022-03-20 20:27:21 -0400

    user is able to add new thoughts/reactions

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import Auth from '../../utils/auth';
 
 const Header = () => {
-
   const logout = event => {
     event.preventDefault();
     Auth.logout();
@@ -23,9 +23,9 @@ const Header = () => {
               <a href="/" onClick={logout}>
                 Logout
               </a>
-              </>
-            ) : (
-              <>
+            </>
+          ) : (
+            <>
               <Link to="/login">Login</Link>
               <Link to="/signup">Signup</Link>
             </>
